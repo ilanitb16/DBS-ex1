ccb84ba6ebf453ab11dea0ef9c3fc27efd04a794
ilanitb16 <ilanit.berdichevski@gmail.com>
2025-04-09 01:06:37 +0300

    questions 1-3 done

---
+++ q2.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="f1_data",
+        port='3306',
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("""
+        SELECT Driver
+        From drivers 
+        Where Nationality = "ITA";
+    """)
+
+    print(', '.join(str(row) for row in cursor.fetchall()))
