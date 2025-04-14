20bd2f9e0bc7895e14d004ad65134da83825615a
ilanitb16 <ilanit.berdichevski@gmail.com>
2025-04-14 11:29:18 +0300

    solved question 6

--- q6.py
+++ q6.py
@@ -11,7 +11,11 @@ if __name__ == '__main__':
 
     cursor = mydb.cursor()
     cursor.execute("""
-    
+    SELECT DISTINCT w1.`Grand Prix` AS GP1, w2.`Grand Prix` AS GP2, w1.Laps
+    FROM winners as w1, winners as w2
+    WHERE w1.Laps = w2.Laps
+    AND w1.Laps > 120
+    AND w1.`Grand Prix`< w2.`Grand Prix`
  """)
 
     print(', '.join(str(row) for row in cursor.fetchall()))
