15a944e7261346455d4b8a245a828041696aa4d2
ilanitb16 <ilanit.berdichevski@gmail.com>
2025-04-14 14:19:51 +0300

    question 9 completed

---
+++ q9.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+if __name__ == '__main__':
+     mydb = mysql.connector.connect(
+     host="localhost",
+     user="root",
+     password="root",
+     database="f1_data",
+     port='3306',
+     )
+
+     cursor = mydb.cursor()
+     cursor.execute("""
+     SELECT drivers.Nationality, 
+     
+         (SELECT AVG(d2.PTS)
+          FROM drivers d2
+          WHERE d2.Nationality = drivers.Nationality) AS avg_pts,
+     
+         (SELECT MIN(f.Time) AS min_time
+          FROM fastest_laps f, drivers d2
+          WHERE f.Driver = d2.Driver 
+            AND d2.Nationality = drivers.Nationality
+         ) AS min_time,
+     
+         (SELECT MAX(w.Date)
+          FROM winners w, drivers d3
+          WHERE w.Winner = d3.Driver 
+            AND d3.Nationality = drivers.Nationality
+         ) AS latest
+     
+     FROM drivers
+     GROUP BY drivers.Nationality
+      """)
+
+     print(', '.join(str(row) for row in cursor.fetchall()))
+
+
+
+
+
