4c7f4c1538169a665cd2bf2fbceb87fe35cc7cd5
ilanitb16 <ilanit.berdichevski@gmail.com>
2025-04-14 11:17:14 +0300

    questions 1-5 complete

---
+++ q6.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+if __name__ == '__main__':
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="f1_data",
+        port='3306',
+    )
+
+    cursor = mydb.cursor()
+    cursor.execute("""
+    
+ """)
+
+    print(', '.join(str(row) for row in cursor.fetchall()))
