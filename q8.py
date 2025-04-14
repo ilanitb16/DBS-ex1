7212436182f8368e5f3d8199cd506780b30cbfd4
ilanitb16 <ilanit.berdichevski@gmail.com>
2025-04-14 11:53:18 +0300

    solved question 8

---
+++ q8.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+if __name__ == '__main__':
+     mydb = mysql.connector.connect(
+     host="localhost",
+     user="root",
+     password="root",
+     database="f1_data",
+     port='3306',
+     )
+
+     cursor = mydb.cursor()
+     cursor.execute("""
+       SELECT 
+             (SELECT SUM(PTS) FROM teams WHERE Team = 'Ferrari') -
+             (SELECT SUM(PTS) FROM teams WHERE Team = 'Maserati') AS diff;
+      """)
+
+     print(', '.join(str(row) for row in cursor.fetchall()))
+
+
+
+
+
